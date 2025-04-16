0c8cdf09b4fd88c176fdf6de6c6874f067af0904
Guillaume <guillaume.valadas@gmail.com>
2025-04-16 11:08:25 +0200

    Add dependency injection

---
+++ src/DependencyInjection/AiContextExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AiContextBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class AiContextExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.yaml');
+    }
+}
