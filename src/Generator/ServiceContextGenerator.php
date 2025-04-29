7cbaa331602be7401c961e3353d5ead2e1e31559
Guillaume <guillaume.valadas@gmail.com>
2025-04-29 15:01:32 +0200

    Refactor ServiceContextGenerator, now extending absctract class

--- src/Generator/ServiceContextGenerator.php
+++ src/Generator/ServiceContextGenerator.php
@@ -1,37 +1,21 @@
 <?php
-
 namespace AiContextBundle\Generator;
 
-use Symfony\Component\Finder\Finder;
 use ReflectionClass;
 use ReflectionMethod;
 use ReflectionParameter;
 
-class ServiceContextGenerator
+class ServiceContextGenerator extends AbstractContextGenerator
 {
     public function __construct(
         private readonly array $servicePaths
-    ) {
-    }
+    ) {}
 
-    /**
-     *
-     * @return array
-     */
     public function generate(): array
     {
-        $finder = new Finder();
-        $finder->files()->in($this->servicePaths)->name('*.php');
-
         $results = [];
 
-        foreach ($finder as $file) {
-            $className = $this->getClassFromFile($file->getRealPath());
-
-            if (!$className || !class_exists($className)) {
-                continue;
-            }
-
+        foreach ($this->findClasses($this->servicePaths) as $className) {
             $reflection = new ReflectionClass($className);
 
             if ($reflection->isAbstract() || $reflection->isInterface() || $reflection->isTrait()) {
@@ -54,28 +38,16 @@ class ServiceContextGenerator
         return [
             'name'       => $method->getName(),
             'returnType' => $method->getReturnType()?->__toString() ?? 'mixed',
-            'parameters' => array_map(fn(ReflectionParameter $param) => [
-                'name'       => $param->getName(),
-                'type'       => $param->getType()?->__toString() ?? 'mixed',
-                'hasDefault' => $param->isDefaultValueAvailable(),
-                'default'    => $param->isDefaultValueAvailable() ? $param->getDefaultValue() : null,
-            ], $method->getParameters()),
-            'doc'        => $method->getDocComment(),
+            'parameters' => array_map(
+                fn(ReflectionParameter $param) => [
+                    'name'       => $param->getName(),
+                    'type'       => $param->getType()?->__toString() ?? 'mixed',
+                    'hasDefault' => $param->isDefaultValueAvailable(),
+                    'default'    => $param->isDefaultValueAvailable() ? $param->getDefaultValue() : null,
+                ],
+                $method->getParameters()
+            ),
+            'doc' => $method->getDocComment(),
         ];
-    }
-
-    private function getClassFromFile(string $filePath): ?string
-    {
-        $contents = file_get_contents($filePath);
-
-        if (!preg_match('/namespace\s+([^;]+);/', $contents, $namespaceMatch)) {
-            return null;
-        }
-
-        if (!preg_match('/class\s+([^\s]+)/', $contents, $classMatch)) {
-            return null;
-        }
-
-        return $namespaceMatch[1] . '\\' . $classMatch[1];
     }
 }
