9ea630b758d7e58af09ddd173d814d11998c42e0
Guillaume <guillaume.valadas@gmail.com>
2025-05-06 18:05:30 +0200

    Fix phpstan

--- src/Generator/EventContextGenerator.php
+++ src/Generator/EventContextGenerator.php
@@ -8,12 +8,14 @@ use Symfony\Contracts\EventDispatcher\Event;
 class EventContextGenerator extends AbstractContextGenerator
 {
     public function __construct(
+        /** @var string[] $eventPaths */
         private readonly array $eventPaths,
     ) {
     }
 
     public function generate(): array
     {
+        /** @var class-string[] $classes */
         $classes = $this->findClasses($this->eventPaths);
         $events = [];
 
@@ -27,6 +29,10 @@ class EventContextGenerator extends AbstractContextGenerator
         return $events;
     }
 
+    /**
+     * @param class-string $fqcn
+     * @return array<string, string>
+     */
     private function extractClassProperties(string $fqcn): array
     {
         $reflection = new ReflectionClass($fqcn);
--- tests/EventContextGeneratorTest.php
+++ tests/EventContextGeneratorTest.php
@@ -14,6 +14,7 @@ class EventContextGeneratorTest extends TestCase
 
         $result = $generator->generate();
 
+        /** @phpstan-ignore-next-line */
         $this->assertIsArray($result);
         $this->assertNotEmpty($result);
 
