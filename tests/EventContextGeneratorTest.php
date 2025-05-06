6b93320683e360f12d72d5f13bbf6a0728248dad
Guillaume <guillaume.valadas@gmail.com>
2025-05-06 17:57:14 +0200

    Add tests for EventContextGenerator

---
+++ tests/EventContextGeneratorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AiContextBundle\Tests;
+
+use AiContextBundle\Generator\EventContextGenerator;
+use PHPUnit\Framework\TestCase;
+
+class EventContextGeneratorTest extends TestCase
+{
+    public function testGenerateReturnsExpectedEventStructure(): void
+    {
+        $eventPath = __DIR__ . '/fixtures/Event';
+        $generator = new EventContextGenerator([$eventPath]);
+
+        $result = $generator->generate();
+
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result);
+
+        foreach ($result as $event) {
+            $this->assertArrayHasKey('class', $event);
+            $this->assertArrayHasKey('properties', $event);
+            $this->assertIsArray($event['properties']);
+            $this->assertStringContainsString('AiContextBundle\\', $event['class']);
+        }
+    }
+}
